f418f2ca53037767df8bb17d0d8ee1e622212048
Emmet O'Grady <emmet789@gmail.com>
2023-07-09 12:25:38 +0100

    Clone zsh-autosuggestions

--- install.sh
+++ install.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
-# Based on the tutorial here:
+# See these for inspiration:
 # https://bea.stollnitz.com/blog/codespaces-terminal/
+# https://burkeholland.dev/posts/codespaces-dotfiles/
 
-create_symlinks() {
-    # Get the directory in which this script lives.
-    script_dir=$(dirname "$(readlink -f "$0")")
+#!/bin/sh
 
-    # Get a list of all files in this directory that start with a dot.
-    files=$(find -maxdepth 1 -type f -name ".*")
-
-    # Create a symbolic link to each file in the home directory.
-    for file in $files; do
-        name=$(basename $file)
-        echo "Creating symlink to $name in home directory."
-        rm -rf ~/$name
-        ln -s $script_dir/$name ~/$name
-    done
+zshrc() {
+    echo "==========================================================="
+    echo "             cloning zsh-autosuggestions                   "
+    echo "-----------------------------------------------------------"                    
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    echo "==========================================================="
+    echo "                  Import zshrc                             "
+    echo "-----------------------------------------------------------"
+    cat .zshrc > $HOME/.zshrc
 }
 
-create_symlinks
+zshrc
