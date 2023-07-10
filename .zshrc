0256c10f59a5a8fb3bb780e0f2bf4704354dc331
Emmet O'Grady <emmet789@gmail.com>
2023-07-10 11:32:33 -0600

    Set theme

--- .zshrc
+++ .zshrc
@@ -1,8 +1,13 @@
 export ZSH="${HOME}/.oh-my-zsh"
 
-ZSH_THEME="agnoster"
+#ZSH_THEME="agnoster"
+ZSH_THEME="eastwood"
 
 # Plugins.
 plugins=(aliases git zsh-autosuggestions z docker-compose)
 
 source $ZSH/oh-my-zsh.sh
+
+# Node Version Manager stuff
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
--- install.sh
+++ install.sh
@@ -11,6 +11,8 @@ zshrc() {
     echo "             cloning zsh-autosuggestions                   "
     echo "-----------------------------------------------------------"                    
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
     echo "==========================================================="
     echo "                  Import zshrc                             "
     echo "-----------------------------------------------------------"
